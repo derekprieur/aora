4d6862115b2c58abf87d6a2d337bf81e32b85a4c
Derek Prieur <DPrieur123@gmail.com>
2024-04-12 17:01:50 -0400

    update

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react'
 import { SplashScreen, Stack } from 'expo-router'
 import { useFonts } from 'expo-font'
+import GlobalProvider from '../context/GlobalProvider'
 
 SplashScreen.preventAutoHideAsync();
 
@@ -30,32 +31,34 @@ const RootLayout = () => {
     }
 
     return (
-        <Stack>
-            <Stack.Screen
-                name='index'
-                options={{
-                    headerShown: false
-                }}
-            />
-            <Stack.Screen
-                name='(auth)'
-                options={{
-                    headerShown: false
-                }}
-            />
-            <Stack.Screen
-                name='(tabs)'
-                options={{
-                    headerShown: false
-                }}
-            />
-            {/* <Stack.Screen
+        <GlobalProvider>
+            <Stack>
+                <Stack.Screen
+                    name='index'
+                    options={{
+                        headerShown: false
+                    }}
+                />
+                <Stack.Screen
+                    name='(auth)'
+                    options={{
+                        headerShown: false
+                    }}
+                />
+                <Stack.Screen
+                    name='(tabs)'
+                    options={{
+                        headerShown: false
+                    }}
+                />
+                {/* <Stack.Screen
                 name='/search/[query]'
                 options={{
                     headerShown: false
                 }}
             /> */}
-        </Stack>
+            </Stack>
+        </GlobalProvider>
     )
 }
 
