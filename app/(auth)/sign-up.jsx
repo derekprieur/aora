468c82dad75a23bb1800e8f3129a0e54dacbf461
Derek Prieur <DPrieur123@gmail.com>
2024-04-12 16:46:47 -0400

    update

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,18 +1,33 @@
-import { View, Text, ScrollView, Image } from 'react-native'
+import { View, Text, ScrollView, Image, Alert } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from '../../constants'
 import FormField from '../../components/FormField'
 import CustomButton from '../../components/CustomButton'
-import { Link } from 'expo-router'
+import { Link, router } from 'expo-router'
 import { createUser } from '../../lib/appwrite'
 
 const SignUp = () => {
     const [form, setForm] = useState({ email: '', password: '', username: '' })
     const [isSubmitting, setIsSubmitting] = useState(false)
 
-    const submit = () => {
-        createUser()
+    const submit = async () => {
+        if (!form.email || !form.password || !form.username) {
+            Alert.alert('Error', 'Please fill in all fields')
+            return
+        }
+        try {
+            setIsSubmitting(true)
+            const result = await createUser(form.email, form.password, form.username)
+
+            // Set to global state
+
+            router.replace('/home')
+        } catch (error) {
+            Alert.alert('Error', error.message)
+        } finally {
+            setIsSubmitting(false)
+        }
     }
 
     return (
@@ -47,7 +62,7 @@ const SignUp = () => {
                         otherStyles='mt-7'
                     />
                     <CustomButton
-                        title='Sign In'
+                        title='Sign Up'
                         handlePress={submit}
                         containerStyles='mt-7'
                         isLoading={isSubmitting}
@@ -59,7 +74,7 @@ const SignUp = () => {
                         <Link
                             href='/sign-in'
                             className='text-lg font-psemibold text-secondary'
-                        >Sign Up</Link>
+                        >Sign In</Link>
                     </View>
                 </View>
             </ScrollView>
