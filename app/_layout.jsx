51cb9cad11c04c9a46f61cf9aae5b7142db1d950
Derek Prieur <DPrieur123@gmail.com>
2024-04-12 10:23:39 -0400

    update

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,21 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Stack } from 'expo-router'
 
 const RootLayout = () => {
     return (
-        <View style={styles.container}>
-            <Text style={{ color: 'white' }}>RootLayout</Text>
-        </View>
+        // <>
+        //     <Text>Header</Text>
+        //     <Slot />
+        //     <Text>Footer</Text>
+        // </>
+        <Stack>
+            <Stack.Screen
+                name='index'
+                options={{
+                    headerShown: false
+                }}
+            />
+        </Stack>
     )
 }
 
 export default RootLayout
-
-const styles = StyleSheet.create({
-    container: {
-        display: 'flex',
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    }
-})
