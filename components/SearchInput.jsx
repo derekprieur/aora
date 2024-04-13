7c083870d55135284c6aac42973ac1844e7e5a45
Derek Prieur <DPrieur123@gmail.com>
2024-04-13 10:31:24 -0400

    update

--- components/SearchInput.jsx
+++ components/SearchInput.jsx
@@ -3,9 +3,9 @@ import React, { useState } from 'react'
 import { icons } from '../constants'
 import { usePathname, router } from 'expo-router'
 
-const SearchInput = () => {
+const SearchInput = ({ initialQuery }) => {
     const pathName = usePathname()
-    const [query, setQuery] = useState('')
+    const [query, setQuery] = useState(initialQuery || '')
 
     return (
         <View className='w-full h-16 px-4 bg-black-100 border-2 border-black-200 rounded-2xl focus:border-secondary items-center flex-row space-x-4'>
